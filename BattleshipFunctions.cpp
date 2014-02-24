e4e7d32a7d7a99953ef1a08063d3f73969c55b38
Michele Burns <mburns@starcraft.highpoint.edu>
2014-02-23 22:37:48 -0500

    "delivery ready commit of stuff

--- BattleshipFunctions.cpp
+++ BattleshipFunctions.cpp
@@ -1,3 +1,10 @@
+
+/* Triad Development Group
+ * Battleship console game
+ *
+ * BattleshipFunctions.cpp  - Module 1 (Intilization) 
+ *
+ */
 #include<iostream>
 #include<ctime>
 #include<climits>
@@ -13,19 +20,142 @@
 using namespace std;
 
 //Function Declarations
+/* Welcome:
+ *
+ * This function is called in the main routine in Battleship.cpp, this function will act as the
+ * starting point for the entire game of Battleship. Calling all other needed functions. This function 
+ * will display the main menu. The function asks the user to input a number which will direct them to
+ * another function.
+ *
+ * Pre-condition: The functions called have already been defined. This function requires no parameters.
+ *
+ * Post-condition: Every other function either falls back to the welcome function, thus displaying the 
+ * main menu or the has exited the program elsewhere.
+ */
 void Welcome();
+/*NewGame:
+ *
+ * This function is called when the user wishes to begin a new game. The function calls createGrid
+ * which creates a ten by ten grid (2-D array) on which the ships will be placed and shots will be fired
+ * both for the user and the cpu. The function prompts the user for their name and input in the form of
+ * character,integer,character,integer on where they would like to place all 5 of their ships. 
+ * The function then calls RandomPlacement, so that the cpu's ships can be placed, then it calls 
+ * playGame and passes playGame the user inputed name, and the two grids with the ships already placed.
+ *
+ * Pre-condition: The functions have been correctly defined. This function requires no parameters.
+ *
+ * Post-condition: ...
+ *
+ */
 void NewGame();
+/*LoadGame:
+ *
+ * This function handles loading a function from a saved file within the current directory entitiled
+ * "saved.txt" it creates locally variables to hold the player's name and then two grids (2-D arrays)
+ * to hold each of the grids from the saved file. This function will then call the playGame 
+ * function with the information loaded from the saved file.
+ *
+ * Pre-condition: This function assumes that there is something in the file, in the correct format to
+ * read in the name and the two grids. 
+ *
+ * Post-condition: The function correctly hands off the name of the player and the two grids to the 
+ * playGame function.
+ *
+ */
 void LoadGame();
-void InitializeGame();
+/*PrintHelpGuide:
+ *
+ * This function prints the help guide, for further information about this function please see comment
+ * in reference.cpp.
+ *
+ * Pre-Condition: The function has no parameters. 
+ *
+ * Post-Condition: The helpguide was printed correctly. The function does not return anything.
+ *
+ */
 void PrintHelpGuide();
+/*PrintHighScores:
+ *
+ * This function prints the high scores, for further information about this function please see comment
+ * in reference.cpp.
+ *
+ * Pre-Condition: This function has no parameters.
+ *
+ * Post-Condition: The high scores were printed correctly. The function does not return anything.
+ *
+ */
 void PrintHighScores();
+/*RandomPlacement:
+ *
+ * This function will create local variables to handle the processing of placing the 5 needed ships 
+ * randomly onto the cpugrid. It does this more quickly by using a start location equation.By this 
+ * equation, the odds that that the ship placement will be valid are increased, because the equation 
+ * reduces the possibily that the ship will be placed in a location it does not fit.
+ *
+ * Pre-Condition: This function is correctly passed a grid (2-D array) loaded with '.' in every spot.
+ *
+ * Post-Condition: This function will have correctly placed the ships onto the board it was given.
+ * ...
+ *
+ */
 void RandomPlacement(char [10][10]);
+/*verifyPlacement: 
+ *
+ * The function will verify that the coordinates passed to it are infact valid. It does this by checking
+ * that the character entered is from A to J and the integer is from 1 to 10, then checking wether
+ * the ship is being placed either horizontally or vertically, then checking to make sure the 
+ * coordinates were correct for the length of the ship, and then finally checking to make sure that the
+ * spaces are not already occupied (meaning they contain the value '.') The function returns either 
+ * true or false depending if the ships placement is valid.  
+ *
+ * Pre-condition: The fucntion is correctly handed, the length of the ship, the charachter and integer 
+ * for the starting point of the ship and the character and integer for the ending point of the ship, 
+ * along with the grid that it is verifying. 
+ *
+ * Post-Condition: The function returns a boolean value.
+ *
+ *
+ */
+
 bool verifyPlacement(int, char, int, char, int, char [10][10]); //length and coordinates and grid
+/*BubbleSort:
+ *
+ *  The bubble sort function sorts the items in the array of integers from highest to lowest using a 
+ *  simple bubble sort algoritm and for loops. When a switch/swap is made with items in the array of 
+ *  integers, the same switch/swap is made in the array of strings so the user name still corresponds 
+ *  to their high score
+ *
+ * Pre-Condition: an array of integers, an array of strings, and one integer. The array of integers 
+ * corresponds to all of the high scores that have been saved to the file highscores.txt, the array of 
+ * strings corresponds to all of the user's names that have been saved to the file highscores.txt, 
+ * and the integer refers to the size of each array. Both arrays will always be the same size since 
+ * there will always be a name saved with a score to the file highscores.txt and the score in the 
+ * array corresponds to the name in the array of strings in the same array position.
+ *
+ * Post-Condition: These 3 parameters are passed by reference. The bubble sort function is only 
+ * called by the PrintHighScores function.
+ *
+ */
 void BubbleSort(int list1[], string list2[], int size);
+/*playGame:
+ *
+ * The function is located in Module2Main.cpp, for further information about the nature of this function
+ * please see that file, a brief synopsis follows. This function is the primary module 2 handler, which
+ * is used to play the game battleship. It handles the taking of turns between the user and the cpu.
+ *
+ * Pre-Condition: The fucntion expects to be correctly handed the name of the player as well as 
+ * the usergrid and the cpugrid, each of which contain the ships correctly placed on the grid.
+ *
+ * Post-Condition: The function returns an int, a 0 for cpu win and a 1 for a player win.
+ *
+ */
 int playGame(string name, char [10][10], char [10][10]);
-//primary module 2 handler to define gameplay
-//expects two grids, the usergrid and cpugrid
-//returns nothing
+
+/*Placement Struct:
+ *
+ * The Placement struct handles the necessary arrays and their definitions for the for loops. 
+ *
+ */
 //Struct Declaration:
 struct Placement{
   int lengtharray [5] = {5,4,3,3,2};
@@ -119,8 +249,11 @@ void NewGame ()
 }
 
 bool verifyPlacement (int length, char x1, int y1, char x2, int y2, char usergrid[10][10])
-{
-   if(x1!=x2 && y1!=y2){
+{  if(!(x>=65&&x<=74)){
+       return false;
+   }else if(!(y>=1&&y<=10)){
+      return false;
+   }else if(x1!=x2 && y1!=y2){
       return false;
    }else if(x1==x2 && (y2-y1+1)!=length){
       return false;
